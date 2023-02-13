e569403d9de8319d6f45f5299fccb3d6c52ddfbd
KartikeyPushkarna <kartikeypushkarna1211@gmail.com>
2023-02-13 00:28:28 -0800

    first file

---
+++ 01hello/main.go
@@ -0,0 +1,16 @@
+package main
+
+import "fmt"
+
+//Printing functions=>
+func main() {
+	fmt.Println("hello from kp")
+	fmt.Printf("hello from kp")
+	fmt.Print("hello from kp")
+	a := fmt.Sprint("hello from kp")
+	b := fmt.Sprintf("hello from kp")
+	c := fmt.Sprintln("hello from kp")
+	fmt.Print(a)
+	fmt.Print(b)
+	fmt.Print(c)
+}
